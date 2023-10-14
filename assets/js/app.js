c487ca007258dd24c7ac0de36e9f8cf294eca4a1
ZaprostoNeprosto <alexandrosmur@gmail.com>
2023-10-14 15:56:58 +0300

    feat: fixed header, smooth scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("#header");
+    const intro = document.querySelector("#intro");
+    let scrollOffset = window.scrollY;
+
+    // Fixed Header
+    checkScroll(scrollOffset);
+
+    window.addEventListener("scroll", function() {
+        scrollOffset = window.scrollY;
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= intro.clientHeight) {
+            header.classList.add("fixed");
+        } else {
+            header.classList.remove("fixed");
+        }
+    }
+
+    // Smooth Scroll
+    const scrollLinks = document.querySelectorAll("[data-scroll]");
+
+    scrollLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            const blockId = link.getAttribute("data-scroll");
+            const targetElement = document.querySelector(blockId);
+            const blockOffset = targetElement.getBoundingClientRect().top + window.scrollY;
+
+            document.querySelectorAll("#nav a").forEach(function(navLink) {
+                navLink.classList.remove("active");
+            });
+
+            link.classList.add("active");
+
+            window.scrollTo({
+                top: blockOffset,
+                behavior: "smooth"
+            });
+        });
+    });
+
 
     // Menu nav toggle
     const navToggle = document.querySelector("#nav_toggle");
